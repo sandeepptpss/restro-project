03248743dd2b77af0fcba06d33bba41093428e0d
sandeepptpss <sandeepptpss@gmail.com>
2023-04-21 15:56:00 +0530

    form validation

---
+++ src/components/Update.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="restro-header-page" >
+        <Header/>
+    </div>
+<div class="restro-Home-page">
+    <h2>WeleCome on Restaurant Update  Page </h2>
+</div>
+</template>
+<script>
+import Header from './Header.vue';
+export default{
+    name:"Update",
+    components:{
+        Header
+    },
+    mounted(){
+        console.log('mounted')
+        let User = localStorage.getItem('user-info');
+        if(!User){
+            this.$router.push({SignUp:'SignUp'})
+        }
+    }
+}
+
+</script>
